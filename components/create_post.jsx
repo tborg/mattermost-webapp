b4271ddcfa193ef1e167a53d2de39a00f4bea1a7
Harrison Healey <harrisonmhealey@gmail.com>
2017-12-11 16:32:13 -0500

    Stopped using dangerouslySetInnerHtml for emoji icon

--- components/create_post.jsx
+++ components/create_post.jsx
@@ -24,15 +24,15 @@ import * as UserAgent from 'utils/user_agent.jsx';
 import * as Utils from 'utils/utils.jsx';
 import store from 'stores/redux_store.jsx';
 
+import ConfirmModal from 'components/confirm_modal.jsx';
 import EmojiPickerOverlay from 'components/emoji_picker/emoji_picker_overlay.jsx';
-
-import ConfirmModal from './confirm_modal.jsx';
-import FilePreview from './file_preview.jsx';
-import FileUpload from './file_upload.jsx';
-import MsgTyping from './msg_typing.jsx';
-import PostDeletedModal from './post_deleted_modal.jsx';
-import Textbox from './textbox.jsx';
-import TutorialTip from './tutorial/tutorial_tip.jsx';
+import FilePreview from 'components/file_preview.jsx';
+import FileUpload from 'components/file_upload.jsx';
+import MsgTyping from 'components/msg_typing.jsx';
+import PostDeletedModal from 'components/post_deleted_modal.jsx';
+import EmojiIcon from 'components/svg/emoji_icon';
+import Textbox from 'components/textbox.jsx';
+import TutorialTip from 'components/tutorial/tutorial_tip.jsx';
 
 const Preferences = Constants.Preferences;
 const TutorialSteps = Constants.TutorialSteps;
@@ -713,10 +713,9 @@ export default class CreatePost extends React.Component {
                         rightOffset={15}
                         topOffset={-7}
                     />
-                    <span
+                    <EmojiIcon
                         id='emojiPickerButton'
                         className={'icon icon--emoji ' + (this.state.showEmojiPicker ? 'active' : '')}
-                        dangerouslySetInnerHTML={{__html: Constants.EMOJI_ICON_SVG}}
                         onClick={this.toggleEmojiPicker}
                     />
                 </span>
